dfa91484c051510d998cbdd7ba9e91ae97ba90b9
Alex Chip <alexchip32@gmail.com>
2019-03-14 16:04:53 -0600

    Primer Commit Configurando el servidor

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const morgan = require('morgan')
+const multer = require('multer')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+
+const viewDir = `${__dirname}/views`
+const publicDir = express.static(`${__dirname}/public`)
+const routes = require('./routes')
+
+// Initializations
+const app = express()
+
+// Settings
+app
+  // .set('port', port)
+  .set('views', viewDir)
+  .engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: `${__dirname}/views/layouts`,
+    partialsDir: `${__dirname}/views/partials`,
+    extname: '.hbs'
+  }))
+  .set('view engine', '.hbs')
+
+// Middlewares
+
+const storage = multer.diskStorage({
+  destination: `${__dirname}/public/img/uploads`,
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + path.extname(file.originalname))
+  }
+})
+
+app
+  .use(morgan('dev'))
+  .use(multer({ storage }).single('image'))
+  .use(express.json())
+  .use(express.urlencoded({ extended : false }))
+
+// Routes
+app
+  .use(routes)
+
+// Public
+app
+  .use(publicDir)
+
+module.exports = app
